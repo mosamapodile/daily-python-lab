9e7f0423f5aeadd5117604c756e59ea62e1a9b88
Mogau Mapodile <momapswgc025@student.wethinkcode.co.za>
2026-02-17 08:35:58 +0200

    initializing skeleton folders

---
+++ scripts/day_01_calculator.py
@@ -0,0 +1,10 @@
+# must be able to add , subtract , devide , multiply 
+
+def add(a,b):
+    return a + b 
+
+def subtract(a,b):
+    return a - b or b - a 
+
+def devide(a,b): # we can not devide a number by 0
+    if b or a 
