32469b384104ea798104d2c33807f3aaa059e20a
Mogau Mapodile <momapswgc025@student.wethinkcode.co.za>
2026-02-18 14:44:06 +0200

    implement main

--- scripts/day_01_calculator.py
+++ scripts/day_01_calculator.py
@@ -4,7 +4,17 @@ def add(a,b):
     return a + b 
 
 def subtract(a,b):
-    return a - b or b - a 
+    return a - b 
 
 def devide(a,b): # we can not devide a number by 0
-    if b or a 
+    if b != 0:
+        return a/b
+    
+def multiplication(a,b):
+    return a * b
+
+def square(a,b):
+    return a**b
+
+def main ():
+    pass 
